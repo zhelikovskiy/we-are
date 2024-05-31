eebaf999a7b38f753e3f500cea9b9068e8219165
Artem Zhelikovskiy <zhelikovskiy.artem@gmail.com>
2024-05-31 16:11:56 +0300

    add cors

--- src/app.js
+++ src/app.js
@@ -1,9 +1,17 @@
 import express from 'express';
 import authRouter from './routes/auth-router.js';
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
+app.use(
+	cors({
+		origin: ['http://localhost:3000', 'http://localhost:3001'],
+		credentials: true,
+		methods: ['GET', 'POST', 'PUT', 'DELETE'],
+	})
+);
 
 app.use('/api/auth', authRouter);
 
