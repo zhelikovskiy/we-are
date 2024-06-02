05e79a3f01fe02bcb9f9cd2b95eb24e2f935d3f1
Artem Zhelikovskiy <zhelikovskiy.artem@gmail.com>
2024-06-02 14:10:40 +0300

    implement database through mongoose

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
 import * as http from 'http';
 import app from './src/app.js';
 import socket from './src/socket.js';
+import connectDb from './src/db.js';
 
 const PORT = process.env.PORT || 3001;
 
 const server = http.createServer(app);
 
 try {
-	server.listen(PORT, () => {
-		console.log(`Server is running on 'http://localhost:${PORT}'`);
-	});
+	connectDb().then(() => {
+		server.listen(PORT, () => {
+			console.log(`Server is running on 'http://localhost:${PORT}'`);
+		});
 
-	socket.initSocket(server);
+		socket.initSocket(server);
+	});
 } catch (error) {
 	console.error(error);
 }
